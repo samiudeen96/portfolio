c7f743a91218535c67396027d02bbf7d36723561
Samiudeen2021 <sami@hasnatech.onmicrosoft.com>
2025-04-27 06:55:31 +0530

    'update'

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,22 +2,23 @@ import React from "react";
 import dp from "../assets/dp2.jpg";
 import { useTypewriter, Cursor } from "react-simple-typewriter";
 
-
 const About = () => {
-    const [text] = useTypewriter({
-      words: ["Front-End Developer"],
-      loop: {},
-      typeSpeed: 70,
-      deleteSpeed: 70,
-    });
+  const [text] = useTypewriter({
+    words: ["Front-End Developer"],
+    loop: {},
+    typeSpeed: 70,
+    deleteSpeed: 70,
+  });
   return (
     <div className="relative">
-
       <div className="lg:hidden flex items-center">
         <img className="object-cover h-25 w-25 rounded-lg " src={dp} alt="" />
         <div className="ps-5">
           <h2 className="text-2xl mt-1 text-[#141414]">Samiudeen</h2>
-          <p className="text-[#915eff] font-semibold">{text}<Cursor /></p>
+          <p className="text-[#915eff] font-semibold">
+            {text}
+            <Cursor />
+          </p>
           {/* <div className="rounded-md  bordermt-2 mt-2">Frontend Developer</div> */}
         </div>
       </div>
@@ -54,82 +55,109 @@ const About = () => {
       <div className="">
         <p className="mt-10 font-semibold text-lg">What I'm Doing</p>
 
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-10 mt-3">
-          <div className="bg-white rounded-3xl shadow-sm hover:shadow-2xl transition-all duration-300 border border-indigo-100 p-8 relative overflow-hidden">
-            <div className="absolute top-0 left-0 w-24 h-24 bg-gradient-to-br from-indigo-100 to-white rounded-br-3xl z-0"></div>
-            <div className="relative z-10">
-              <div className="flex items-center gap-4 mb-5">
-                <div className="p-3 bg-indigo-100 text-[#915eff] rounded-xl">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="w-6 h-6"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
+        <div className="grid grid-cols-1 md:grid-cols-2 sm:gap-10 gap-5 mt-3">
+          {expIn.map((item) => (
+            <div
+              key={item.name}
+              className={`bg-white rounded-3xl shadow hover:shadow-lg transition-all duration-300 border border-${item.gradientColor} p-6 relative overflow-hidden`}
+            >
+              <div
+                className={`absolute top-0 left-0 w-24 h-24 bg-gradient-to-br from-${item.gradientColor} to-white rounded-br-3xl z-0`}
+              ></div>
+              <div className="relative z-10">
+                <div className="flex items-center gap-4 mb-5">
+                  <div
+                    className={`p-3 bg-${item.gradientColor} text-${item.Color} rounded-xl`}
                   >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M9.75 17L4.5 12l5.25-5m4.5 10l5.25-5-5.25-5"
-                    />
-                  </svg>
+                    {item.category === "frontend" && (
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="w-6 h-6"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          strokeWidth="2"
+                          d="M9.75 17L4.5 12l5.25-5m4.5 10l5.25-5-5.25-5"
+                        />
+                      </svg>
+                    )}
+                    {item.category === "fullstack" && (
+                      <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="w-6 h-6"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M3 7h18M3 12h18m-6 5H3"
+                      />
+                    </svg>
+                    )}
+                  </div>
+                  <h3 className={`text-xl font-bold text-${item.Color}`}>
+                    {item.name}
+                  </h3>
                 </div>
-                <h3 className="text-xl font-bold text-[#915eff]">
-                  Frontend Developer
-                </h3>
+                <p className="text-gray-600 mb-4">{item.desc}</p>
+                <ul className="text-sm text_gray space-y-1">
+                  {item.points.map((li, index) => (
+                    <li key={index}>{li.list}</li>
+                  ))}
+                </ul>
               </div>
-              <p className="text-gray-600 mb-4">
-                I design and build beautiful, responsive interfaces with a focus
-                on performance and accessibility.
-              </p>
-              <ul className="text-sm text-gray-700 space-y-1 mb-6">
-                <li>✔️ HTML / CSS / JavaScript</li>
-                <li>✔️ React / Angular / Tailwind CSS</li>
-                <li>✔️ UI/UX & Responsive Design</li>
-              </ul>
             </div>
-          </div>
-
-          <div className="bg-white rounded-3xl shadow-sm hover:shadow-2xl transition-all duration-300 border border-green-100 p-8 relative overflow-hidden">
-            <div className="absolute top-0 left-0 w-24 h-24 bg-gradient-to-br from-green-100 to-white rounded-br-3xl z-0"></div>
-            <div className="relative z-10">
-              <div className="flex items-center gap-4 mb-5">
-                <div className="p-3 bg-green-100 text-green-600 rounded-xl">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="w-6 h-6"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M3 7h18M3 12h18m-6 5H3"
-                    />
-                  </svg>
-                </div>
-                <h3 className="text-xl font-bold text-green-700">
-                  Backend Developer
-                </h3>
-              </div>
-              <p className="text-gray-600 mb-4">
-                I build robust backend systems, APIs, and database structures
-                that power modern web applications.
-              </p>
-              <ul className="text-sm text-gray-700 space-y-1 mb-6">
-                <li>✔️ Node.js</li>
-                <li>✔️ Express / MongoDb / MySQL</li>
-                <li>✔️ API Integration & Security</li>
-              </ul>
-            </div>
-          </div>
+          ))}
         </div>
       </div>
     </div>
   );
 };
+
+const expIn = [
+  {
+    name: "Front-end Developer",
+    desc: "I design and build beautiful, responsive interfaces with a focus on performance and accessibility.",
+    points: [
+      {
+        list: "✔️ HTML / CSS / JavaScript",
+      },
+      {
+        list: "✔️ React / Angular / Tailwind CSS",
+      },
+      {
+        list: "✔️ UI/UX & Responsive Design",
+      },
+    ],
+    Color: "[#915eff]",
+    gradientColor: "indigo-100",
+    category: "frontend",
+  },
+  {
+    name: "Full-stack Developer",
+    desc: "I design and build beautiful, responsive interfaces with a focus on performance and accessibility.",
+    points: [
+      {
+        list: "✔️ HTML / CSS / JavaScript",
+      },
+      {
+        list: "✔️ React / Angular / Tailwind CSS",
+      },
+      {
+        list: "✔️ UI/UX & Responsive Design",
+      },
+    ],
+    Color: "green-700",
+    gradientColor: "green-100",
+    category: "fullstack",
+  },
+];
 
 export default About;
