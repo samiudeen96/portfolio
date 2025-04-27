9f6a979806fe56367e6b4b972cc41fc71c029dba
Samiudeen2021 <sami@hasnatech.onmicrosoft.com>
2025-04-27 08:29:03 +0530

    'update'

--- src/components/TimelineItem.jsx
+++ src/components/TimelineItem.jsx
@@ -6,15 +6,13 @@ const TimelineItem = ({ item, index }) => {
       <div className="w-[35px] h-[35px]  absolute -left-11 -top-2 flex justify-center items-center">
         <img src={item.icon} className="w-[22px] h-[22px]" alt="" />
       </div>
-      <div className="flex flex-wrap items-center justify-between">
-        <p className="text-md timeline-item-title text-[#915eff] font-semibold">
-          {item.title}
-        </p>
+      <div className="">
         {/* <p className="w-52 md:w-52 lg:w-auto text_gray font-semibold">
           {item.name}
         </p> */}
-        <p className="text_gray font-semibold">
-          {item.name}
+        <p className="text_gray font-semibold">{item.name}</p>
+        <p className="text-md timeline-item-title text-[#915eff] font-semibold">
+          {item.title}
         </p>
       </div>
       <div className="flex items-start justify-between flex-wrap">
