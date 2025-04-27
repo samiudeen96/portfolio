abd01a023ef29e90075a927a97d75183d846b981
Samiudeen2021 <sami@hasnatech.onmicrosoft.com>
2025-04-27 09:06:24 +0530

    'update'

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,9 +1,59 @@
-import React from 'react'
+import React from "react";
 
 const Contact = () => {
   return (
-    <div>Contact</div>
-  )
-}
+    <section>
+      <section className="mapbox">
+        <figure>
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d31398.518481761374!2d79.36359410439539!3d10.356687768910618!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3afffe01f93954dd%3A0x45738afa24f38380!2sAdirampattinam%2C%20Tamil%20Nadu%20614701!5e0!3m2!1sen!2sin!4v1745723988928!5m2!1sen!2sin"
+            width="600"
+            height="450"
+            allowfullscreen=""
+            loading="lazy"
+            referrerpolicy="no-referrer-when-downgrade"
+          ></iframe>
+        </figure>
+      </section>
 
-export default Contact
+      {/* <section className="contact-form">
+        <form action="#" className="form" data-form>
+          <div className="input-wrapper">
+            <input
+              type="text"
+              name="fullname"
+              className="form-input"
+              placeholder="Full name"
+              required
+              data-form-input
+            />
+
+            <input
+              type="email"
+              name="email"
+              className="form-input"
+              placeholder="Email address"
+              required
+              data-form-input
+            />
+          </div>
+
+          <textarea
+            name="message"
+            className="form-input"
+            placeholder="Your Message"
+            required
+            data-form-input
+          ></textarea>
+
+          <button className="form-btn" type="submit" data-form-btn>
+            <ion-icon name="paper-plane"></ion-icon>
+            <span>Send Message</span>
+          </button>
+        </form>
+      </section> */}
+    </section>
+  );
+};
+
+export default Contact;
